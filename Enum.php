864cbc2ea3c22478453b81dd75d75ed35a2f06a4
Mikhail Yarmoluk <m.yarmoluk@kakadu.bz>
2018-06-04 12:18:35 +0300

    Init

---
+++ Enum.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Kakadu\Yii2Enum;
+
+/**
+ * Class    Enum
+ * @package Kakadu\Yii2Enum
+ * @author  Konstantin Timoshenko
+ * @author  Yarmaliuk Mikhail
+ * @version 2.0
+ *
+ * @since   2.0 rename to Enum
+ * @since   1.0 AbstractDictionary
+ */
+abstract class Enum
+{
+    /**
+     * Default item
+     *
+     * @var array
+     */
+    protected static $notSetMessage = ['app', 'Не указано'];
+
+    /**
+     * Enum model attribute name
+     *
+     * @var string|NULL
+     */
+    protected static $attribute = NULL;
+
+    /**
+     * Get all items
+     *
+     * @return array
+     *
+     * @since 2.0 not abstract
+     * @since 1.0 abstract function
+     */
+    public static function all(): array
+    {
+        return [];
+    }
+
+    /**
+     * Enum constructor.
+     *
+     * @return void
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Get all items keys
+     *
+     * @return array
+     */
+    public static function keys(): array
+    {
+        return array_keys(static::all());
+    }
+
+    /**
+     * Get title by vendor
+     *
+     * @param mixed $key
+     *
+     * @return string|NULL
+     */
+    public static function get($key): ?string
+    {
+        $key = \is_object($key) ? $key->{self::$attribute} : $key;
+
+        return static::all()[$key] ?? static::getDefault();
+    }
+
+    /**
+     * Get default item
+     *
+     * @return string
+     */
+    public static function getDefault(): string
+    {
+        [$category, $message] = static::$notSetMessage;
+
+        return \Yii::t($category, $message);
+    }
+
+    /**
+     * If model attribute has key
+     *
+     * @param mixed      $model
+     * @param string|int $key
+     *
+     * @return bool
+     */
+    public static function has($model, $key): bool
+    {
+        if (is_object($model)) {
+            if ($attribute = static::$attribute) {
+                return $model->$attribute == $key;
+            }
+        } else {
+            return $model == $key;
+        }
+
+        return false;
+    }
+
+    /**
+     * If model attribute has key in range
+     *
+     * @param mixed $model
+     * @param array $keys
+     *
+     * @return bool
+     */
+    public static function hasIn($model, array $keys): bool
+    {
+        if (is_object($model)) {
+            if ($attribute = static::$attribute) {
+                return \in_array($model->$attribute, $keys);
+            }
+        } else {
+            return \in_array($model, $keys);
+        }
+
+        return false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,133 @@
+# yii2-enum
+Extension provide very simply use enum for models (and others) in yii2
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist matthew-p/yii2-services "*"
+```
+
+or add
+
+```
+"kakadu-dev/yii2-enum": "@dev"
+```
+
+to the require section of your `composer.json` file.
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by:
+
+Create directory structure for model "User" (only example, not required):
+
+```
+common/  
+    models/    
+        Users/  
+            Enum/
+                UserStatus.php
+            User.php
+            UserQuery.php
+```
+
+UserStatus class example:
+```php
+<?php
+
+namespace common\models\Users\Enum;
+
+use Yii;
+use Kakadu\Yii2Enum;
+
+/**
+ * Class    UserStatus
+ * @package common\models\Users\Enum
+ * @author  
+ * @version 1.0
+ */
+class UserStatus extends Enum
+{
+    const DELETED = 0;
+    const ACTIVE  = 1;
+
+    /**
+     * @inheritdoc
+     */
+    protected static $attribute = 'status';
+
+    /**
+     * @inheritdoc
+     */
+    public static function all(): array
+    {
+        return [
+            self::DELETED => Yii::t('app', 'Deleted'),
+            self::ACTIVE  => Yii::t('app', 'Active'),
+        ];
+    }
+}
+```
+
+And use:
+
+```php
+...
+
+use common\models\Users\Enum\UserStatus;
+...
+
+class User extends ActiveRecord implements IdentityInterface
+{
+    ...
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['status', 'default', 'value' => UserStatus::ACTIVE],
+            ['status', 'in', 'range' => UserStatus::keys()],
+        ];
+    }
+    
+    ...
+}
+```
+
+More examples:
+```php
+$model = new User(['status' => UserStatus::ACTIVE]);
+
+if (UserStatus::has($model, UserStatus::ACTIVE)) {
+    //
+}
+
+// DetailView widget (or GridView)
+DetailView::widget([
+    'model'      => $model,
+    'attributes' => [
+        'id',
+        'name',
+        [
+            'attribute' => 'status',
+            'format'    => 'raw',
+            'filter'    => UserStatus::all(),
+            'value'     => UserStatus::get($model->status),
+        ],
+        ...
+    ],
+])
+
+// In form
+$form->field($model, 'status')->dropDownList(UserStatus::all())
+```
+
+That's all. Check it.
